c603bc13cd859760b376a5cb7b05ada47914f0f8
Shalom <shlomo000@gmail.com>
2024-12-16 18:05:56 +0200

    changes

--- client/src/pages/settings/Settings.tsx
+++ client/src/pages/settings/Settings.tsx
@@ -1,54 +1,111 @@
-import { Input, Label, Text } from '@fluentui/react-components'
-import { FormEvent, useEffect, useState } from 'react'
-import { expenseCategories } from '../../types/expenseCategories'
+import {
+  Button,
+  Input,
+  Label,
+  Spinner
+} from '@fluentui/react-components';
+import { FormEvent, useEffect, useState } from 'react';
+import { expenseCategories } from '../../types/expenseCategories';
 
-const Settings = () => {
-  const handelSubmit = async (e: FormEvent) => {
-    e.preventDefault()
-  }
-  const [state, setState] = useState<{ [key: string]: number }>({})
-  const [isLoading, setIsLoading] = useState(true)
-  const handelChange = (key:string, value: number) => {
-    setState({...state, [key]: value})
-  }
+export default function Settings() {
+  const [state, setState] = useState<{ [key: string]: number }>({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const getSettingsData = async () => {
     try {
-      const res = await fetch('http://localhost:3000/api/settings')
-      const data = await res.json()
-      setState(data)
-      setIsLoading(false)
-    } catch (err) {
-      
-    }
-  }
+      const res = await fetch('http://localhost:3000/api/settings');
+      const data = await res.json();
+      setState(data);
+      setIsLoading(false);
+    } catch (err) {}
+  };
+
+  const updateSettings = async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch('http://localhost:3000/api/settings', {
+        method: 'post',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(state),
+      });
+      const data = await res.json();
+      setState(data);
+      setIsLoading(false);
+    } catch (err) {}
+  };
+
+  const handleChange = (key: string, value: number) => {
+    setState({ ...state, [key]: value });
+  };
 
   useEffect(() => {
+    getSettingsData();
+  }, []);
 
-  }, [])
-  
-
-  return (
+  const hadnleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    await updateSettings();
+  };
+  return isLoading ? (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Spinner size="extra-large" />
+    </div>
+  ) : (
     <div>
-      <Text as='h1'>Settings</Text>
-      <form className='inp-style' onSubmit={handelSubmit}>
-        {expenseCategories.map((e) => (
-          <div>
-            <Label htmlFor={e.id}>{e.display}</Label>
+      <form onSubmit={hadnleSubmit}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            maxWidth: '80%',
+            padding: '1em',
+          }}
+        >
+          <Label htmlFor={'total'}>Total</Label>
+          <Input
+            value={state.total?.toString()}
+            onChange={(e) => handleChange('total', e.target.valueAsNumber)}
+            appearance="underline"
+            type="number"
+            prefix="₪"
+            id={'total'}
+          />
+        </div>
+        {expenseCategories.map((expense) => (
+          <div
+            key={expense.id}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              maxWidth: '80%',
+              padding: '1em',
+            }}
+          >
+            <Label htmlFor={expense.id}>{expense.display}</Label>
             <Input
-              value={state[e.id].total.toString()}
-              onChange={e => handelChange('total', e.target.valueAsNumber)}
+              value={state[expense.id]?.toString()}
+              onChange={(e) => handleChange(expense.id, e.target.valueAsNumber)}
               contentBefore={'₪'}
-              appearance='underline'
-              type='number'
-              size='large'
-              id={e.id}
+              appearance="underline"
+              type="number"
+              size="large"
+              id={expense.id}
             />
           </div>
         ))}
+        <Button appearance="primary" type="submit">
+          Save
+        </Button>
       </form>
     </div>
-  )
+  );
 }
-
-export default Settings
