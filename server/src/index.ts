a364cd6bc3beb2f718beb45daf35146a0ad5c39d
Shalom <shlomo000@gmail.com>
2024-12-16 14:28:12 +0200

    add err handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,22 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import expenseRouter from './routs/expense.rout'
 import settingsRouter from './routs/stting.rout'
+import { connectToMongo } from './config/db'
 
 const app = express()
+
+connectToMongo()
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/expense', expenseRouter)
+
 app.use('/api/stting', settingsRouter)
+
+// error handling
+app.use(async (req: Request, res: Response) => {})
 
 app.listen(3000, ()=> {
   console.log(`[Server] is up and runing no port 3000`)
